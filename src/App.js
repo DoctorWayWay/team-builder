fd8536a019a77ded030a627e1c1b9ded2296c3ea
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-10-12 18:06:25 -0700

    Completed functionality for form inputs. Now when one fills all slots in the form, a new member card gets appended to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // Importing Libraries and StyleSheet
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
+import MemberCard from "./components/MemberCard";
 
 // Importing Components
 import MemberForm from "./components/MemberForm";
@@ -17,7 +18,8 @@ const initialFormData = {
 function App() {
   // State Management
   const [formData, setFormData] = useState(initialFormData);
-  const [teamMembers, setTeamMembers] = useState([]);
+  const [members, setMembers] = useState([]);
+  // const [teamMembers, setTeamMembers] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
   // Update Form Function
   const updateForm = (inputName, inputValue) => {
@@ -30,18 +32,29 @@ function App() {
       email: formData.email.trim(),
       role: formData.role,
     };
+    console.log(newMember);
     // Conditional Warning Message
     if (!newMember.name || !newMember.email || !newMember.role) {
       setErrorMessage("Please fill out the entire form.");
       return;
     }
+    // Add member to list
+    setMembers([...members, newMember]);
   };
 
   // Returing App
   return (
     <div className="App">
       <h1>Member Form</h1>
-      <MemberForm />
+      <MemberForm
+        formValues={formData}
+        error={errorMessage}
+        update={updateForm}
+        submit={submitForm}
+      />
+      {members.map((member) => {
+        return <MemberCard memberInfo={member} />;
+      })}
     </div>
   );
 }
